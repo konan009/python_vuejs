29d481b976ae509041dbe52105b977e4d67902ac
Meljohn <ugaddanmeljohn@gmail.com>
2022-06-12 05:01:19 +0800

    Added Configurations

---
+++ install.sh
@@ -0,0 +1,11 @@
+python manage.py migrate
+python manage.py createcachetable
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+$@
